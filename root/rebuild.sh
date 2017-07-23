3f50949e5ab6d95b72680c92595b655928772541
Max Ohm <maxohm@gmail.com>
2017-07-23 12:13:39 +0300

    ++ copper up

--- root/rebuild.sh
+++ root/rebuild.sh
@@ -4,12 +4,15 @@
 #
 # (c) 2017, maxohm [ at ] gmail.com
 #
-~/clean.sh > /dev/null
+dname=`which dirname`
+home=`$dname $0`
+#
+$home/clean.sh > /dev/null
 #
 mkdir -vp /usr/obj/usr/src/tmp/usr/lib/i18n
 #
 make -j5 buildworld > /dev/null
-make buildkernel KERNCONF=$1 #> /dev/null
+make buildkernel KERNCONF=$1
 make installkernel KERNCONF=$1
 #
 chmod a-x /usr/libexec/sendmail/sendmail
