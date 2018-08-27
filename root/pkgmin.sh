6edff4398d0d183915d4547f74897770d3792faa
Max Ohm <maxohm@gmail.com>
2018-08-27 12:29:43 +0300

    up

--- root/pkgmin.sh
+++ root/pkgmin.sh
@@ -7,8 +7,8 @@
 ping="`which ping` -q -c 1 -W 1"
 ntpq="`which ntpdate` -4 -v -b"
 #
-if $ping ntp1.vniiftri.ru >/dev/null; then
- $ntpq ntp1.vniiftri.ru >> /var/log/ntpd.log
+if $ping ntp1.niiftri.irkutsk.ru >/dev/null; then
+ $ntpq ntp1.niiftri.irkutsk.ru >> /var/log/ntpd.log
 else
  exit 1
 fi
