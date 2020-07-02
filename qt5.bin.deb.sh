12dd75b0f271536fcf4eadd221f06000cd3a8c56
Max Ohm <maxohm@gmail.com>
2020-07-03 00:19:28 +0300

    qt5 bin inst

--- qt5.bin.deb.sh
+++ qt5.bin.deb.sh
@@ -5,7 +5,7 @@
 #
 install="`which apt-get` -fy install"
 #
-$install g++ make python-dev libgl1-mesa-dev
+$install make python-dev libgl1-mesa-dev
 $install qtbase5-dev qtmultimedia5-dev qttools5-dev libqt5serialport5-dev libqt5svg5-dev qtscript5-dev libqt5xmlpatterns5-dev libqt5webengine-data libqt5webengine5 libqt5webenginecore5 libqt5webenginewidgets5 qtwebengine5-dev qtwebengine5-dev-tools qtwebengine5-private-dev
 $install qtcreator valgrind
 #
