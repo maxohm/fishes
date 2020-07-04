69eeff9cf9fb8c7525446aa70de3b74a5d8ad81f
Max Ohm <maxohm@gmail.com>
2020-07-04 13:51:11 +0300

    catshark scripts revision

---
+++ qtwebengine5.deps.deb.sh
@@ -0,0 +1,64 @@
+#!/bin/sh -x
+# (c) 2019, maxohm [ at ] gmail.com
+# (!) requires sudo
+#
+install="`which apt-get` -f install"
+#
+#$install g++ make qt5-qmake qtwebengine5-dev
+$install \
+alsa-base \
+libdbus-1 \
+ffmpeg \
+fontconfig \
+libatk1.0 \
+libavcodec \
+libavformat \
+libavutil \
+libbz2 \
+libcairo2 \
+libdrm \
+libevent \
+libexif \
+libfreetype6 \
+libglib2.0 \
+libharfbuzz \
+libicu \
+libjsoncpp \
+liblzma \
+libmtp \
+libnspr4 \
+libnss3 \
+libopus \
+libpango1.0 \
+libpangocairo-1.0-0 \
+libpci \
+libpng \
+libprotobuf \
+libpulse \
+libsrtp0 \
+libssl \
+libudev \
+libusb \
+libvpx \
+libwebp \
+libx11 \
+libxcomposite \
+libxcursor \
+libxdamage \
+libxext \
+libxfixes \
+libxi \
+libxml2 \
+libxrandr \
+libxrender \
+libxslt1 \
+libxss \
+libxt \
+libxtst \
+minizip \
+perl \
+speex \
+valgrind \
+wdiff \
+yasm \
+zlib1g \
